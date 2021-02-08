4911f9ddc36bf137da20c7f90e2c2ea673065d91
Mateusz Flisikowski <contact@mflisikowski.dev>
2021-02-08 18:39:48 +0100

    update netlify configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,39 @@
+// ─────────────────────────────────────────────────────────────────────────────
+// import
+// ─────────────────────────────────────────────────────────────────────────────
+
+// eslint-disable-next-line import/no-unresolved
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// ─────────────────────────────────────────────────────────────────────────────
+// Component
+// ─────────────────────────────────────────────────────────────────────────────
+
 module.exports = {
   siteMetadata: {
     siteGithub: `https://github.com/nomads-codes/gatsby-starter`,
     siteUrl: `https://nomadscodes-gatsby-starter.netlify.app`,
     siteTitle: `Nomads Codes`,
   },
+  developMiddleware: (app) => {
+    app.use(
+      '/.netlify/functions/',
+      createProxyMiddleware({
+        target: 'http://localhost:9000',
+        pathRewrite: {
+          '/.netlify/functions/': '',
+        },
+      }),
+    );
+  },
   plugins: [
-    { resolve: `gatsby-plugin-netlify-cms` },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/utils/cms.js`,
+        manualInit: true,
+      },
+    },
     { resolve: `gatsby-transformer-sharp` },
     { resolve: `gatsby-plugin-sharp` },
     { resolve: `gatsby-plugin-styled-components` },
