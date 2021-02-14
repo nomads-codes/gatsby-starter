7600154a6aa5cc831d86928f75151c5e360b035f
Mateusz Flisikowski <contact@mflisikowski.dev>
2021-02-14 02:46:15 +0100

    add first hook: useLocalStorage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,32 @@
+// ─────────────────────────────────────────────────────────────────────────────
+// Import
+// ─────────────────────────────────────────────────────────────────────────────
+
+import { useState } from 'react';
+
+// ─────────────────────────────────────────────────────────────────────────────
+// Component
+// ─────────────────────────────────────────────────────────────────────────────
+
+export default function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const storeValue = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(storeValue);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      //
+    }
+  };
+
+  return [storedValue, setValue];
+}
