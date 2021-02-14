b72a7eff67346728ea1285d388ea00dbb159fe9c
Mateusz Flisikowski <contact@mflisikowski.dev>
2021-02-14 02:46:33 +0100

    update aliasses

--- gatsby/onCreateWebpackConfig.js
+++ gatsby/onCreateWebpackConfig.js
@@ -15,6 +15,7 @@ module.exports = ({ plugins, actions }) => {
         '~components': path.resolve(__dirname, '../src/components'),
         '~containers': path.resolve(__dirname, '../src/containers'),
         '~blocks': path.resolve(__dirname, '../src/blocks'),
+        '~hooks': path.resolve(__dirname, '../src/hooks'),
         '~theme': path.resolve(__dirname, '../src/theme'),
         '~utils': path.resolve(__dirname, '../src/utils'),
       },
