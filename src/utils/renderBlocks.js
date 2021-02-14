2d426c9a1ecd41f9203db3d42c618a9b2fe82150
Mateusz Flisikowski <contact@mflisikowski.dev>
2021-02-14 02:32:59 +0100

    add test Hero block

--- src/utils/renderBlocks.js
+++ src/utils/renderBlocks.js
@@ -4,12 +4,18 @@
 
 import React from 'react';
 
+import { HeroBlock } from '~blocks';
+
 // ─────────────────────────────────────────────────────────────────────────────
 // Component
 // ─────────────────────────────────────────────────────────────────────────────
 
 const renderBlocks = (blocks) => {
   return blocks.map((block) => {
+    if (block.type === 'hero') {
+      return <HeroBlock key={block.title} title={block.title} />;
+    }
+
     return null;
   });
 };
