45bec93a0757a4e8bd87859d7e077fdafa2e9733
Mateusz Flisikowski <contact@mflisikowski.dev>
2021-02-21 23:12:53 +0100

    contact page use mdx blocks

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,8 @@ import { graphql } from 'gatsby';
 import React from 'react';
 
 import { RootContainer } from '~containers';
-import { H1, P } from '~components';
+import { renderBlocks } from '~utils';
+import { Main } from '~components';
 
 // ─────────────────────────────────────────────────────────────────────────────
 //  Component
@@ -15,13 +16,19 @@ import { H1, P } from '~components';
 const ContactPage = ({
   data: {
     page: {
-      frontmatter: { layout, meta },
+      frontmatter: { layout, meta, blocks },
     },
+    images: { nodes },
   },
 }) => {
+  const pageBlocksBody = renderBlocks({
+    images: nodes,
+    blocks,
+  });
+
   return (
     <RootContainer meta={meta} layout={layout}>
-      <H1>ContactPage</H1>
+      <Main>{pageBlocksBody}</Main>
     </RootContainer>
   );
 };
@@ -45,6 +52,29 @@ export const query = graphql`
       frontmatter {
         ...LAYOUT_FRAGMENT
         ...META_FRAGMENT
+        blocks {
+          title
+          type
+          mdx
+        }
+      }
+    }
+
+    images: allFile(
+      filter: { absolutePath: { regex: "/contact/" }, extension: { regex: "/(jpg)|(jpeg)|(png)/" } }
+    ) {
+      nodes {
+        id
+        name
+        publicURL
+        childrenImageSharp {
+          fixed(quality: 55) {
+            ...GatsbyImageSharpFixed_noBase64
+          }
+          fluid(maxWidth: 1024, quality: 55) {
+            ...GatsbyImageSharpFluid_noBase64
+          }
+        }
       }
     }
   }
